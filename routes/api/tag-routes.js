f4f8e120792d316e48ac4f27938fd5e66a352fd4
Cheryl Fogerty <cfogerty@aol.com>
2021-05-28 17:17:28 -0500

    final update

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,22 +30,14 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   Tag.findOne({
+    attributes: ['id', 'tag_name'],
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'tag_name'],
-  })
-  // be sure to include its associated Product data
   include: [
     {
       model: Product,
-      attributes: [
-        'id',
-        'product_name',
-        'price',
-        'stock',
-        'category_id'
-      ],
+      attributes: ['id','product_name','price','stock','category_id'],
       through: ProductTag,
       as: 'tagIds'
     }
@@ -115,5 +107,6 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+})
 
 module.exports = router;
